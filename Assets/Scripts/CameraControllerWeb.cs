c2b8f79c4882bd1e48b842cf0c6c16e551a31649
abuchele <anna.buchele@students.olin.edu>
2017-03-25 16:46:31 -0400

    Added In-game camera display!

---
+++ Assets/Scripts/CameraControllerWeb.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControllerWeb : MonoBehaviour
+{
+    public WebCamTexture mCamera = null;
+    public GameObject plane;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Script has been started");
+        plane = GameObject.FindWithTag("WebcamDisplay");
+
+        mCamera = new WebCamTexture();
+        plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+        mCamera.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
